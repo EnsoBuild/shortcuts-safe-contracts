de3986da840c475cd34c4b8c3b56ad064b125e39
Devan Non <devanon@chainstride.capital>
2023-04-24 17:05:13 +0200

    setup with weth

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -11,6 +11,7 @@ struct DeployerResult {
 contract Deployer is Script {
     function run() public returns (DeployerResult memory result) {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
         vm.startBroadcast(deployerPrivateKey);
 
         result.shortcuts = new SafeEnsoShortcuts{salt: "SafeEnsoShortcuts"}();
--- test/SafeEnsoShortcuts.t.sol
+++ test/SafeEnsoShortcuts.t.sol
@@ -1,33 +1,63 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.17;
 
-import {Test} from "../lib/forge-std/src/Test.sol";
+import {Test} from "forge-std/Test.sol";
+import {SafeTestTools, SafeTestLib, SafeInstance, Enum} from "safe-tools/SafeTestTools.sol";
 import {Deployer, DeployerResult} from "../script/Deployer.s.sol";
 import {SafeEnsoShortcuts} from "../src/SafeEnsoShortcuts.sol";
 import {WeirollPlanner} from "./utils/WeirollPlanner.sol";
 
-contract SafeEnsoShortcutsTest is Test {
+contract SafeEnsoShortcutsTest is Test, SafeTestTools {
+    using SafeTestLib for SafeInstance;
+
     SafeEnsoShortcuts shortcuts;
+    SafeInstance safeInstance;
+
+    address alice = makeAddr("alice");
 
     function setUp() public {
+        address deployer = vm.addr(vm.envUint("PRIVATE_KEY"));
+        deal(deployer, 1 ether);
+
         DeployerResult memory result = new Deployer().run();
 
         shortcuts = result.shortcuts;
+
+        safeInstance = _setupSafe();
     }
 
-    function testSafeCanRunShortcut() public {
+    function testSafeCanRunShortcutTransferringERC20() public {
         bytes32[] memory commands = new bytes32[](1);
         commands[0] = WeirollPlanner.buildCommand(
-            SafeEnsoShortcutsTest(address(this)).testSafeCanRunShortcut.selector,
+            weth.transfer.selector,
             0x01, // call
             0x0001ffffffff, // 2 inputs
             0xff, // no output
             address(this)
         );
 
-        bytes[] memory state = new bytes[](1);
-        state[0] = abi.encode(0x1);
+        bytes[] memory state = new bytes[](2);
+        state[0] = abi.encode(alice);
+        state[1] = abi.encode(10 ether);
 
-        shortcuts.executeShortcut(bytes32(0), commands, state);
+        bytes memory data = abi.encodeCall(SafeEnsoShortcuts.executeShortcut, (bytes32(0), commands, state));
+
+        assertEq(weth.balanceOf(address(safeInstance.safe)), 0);
+        assertEq(weth.balanceOf(alice), 0);
+
+        deal(address(weth), address(safeInstance.safe), 10 ether);
+
+        assertEq(weth.balanceOf(address(safeInstance.safe)), 10 ether);
+        assertEq(weth.balanceOf(alice), 0);
+
+        safeInstance.execTransaction({
+            to: address(shortcuts),
+            value: 0 ether,
+            data: data,
+            operation: Enum.Operation.DelegateCall
+        });
+
+        assertEq(weth.balanceOf(address(safeInstance.safe)), 0);
+        assertEq(weth.balanceOf(alice), 10 ether);
     }
 }
