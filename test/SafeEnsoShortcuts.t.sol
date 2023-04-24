02b6187a8e1bb3f401294ee3b844430259c89313
Devan Non <devanon@chainstride.capital>
2023-04-24 14:01:08 +0200

    call script in test

--- test/SafeEnsoShortcuts.t.sol
+++ test/SafeEnsoShortcuts.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.17;
 
 import {Test} from "../lib/forge-std/src/Test.sol";
+import {Deployer, DeployerResult} from "../script/Deployer.s.sol";
 import {SafeEnsoShortcuts} from "../src/SafeEnsoShortcuts.sol";
 import {WeirollPlanner} from "./utils/WeirollPlanner.sol";
 
@@ -9,49 +10,24 @@ contract SafeEnsoShortcutsTest is Test {
     SafeEnsoShortcuts shortcuts;
 
     function setUp() public {
-        // Invoke deploy script
-        shortcuts = new SafeEnsoShortcuts();
+        DeployerResult memory result = new Deployer().run();
+
+        shortcuts = result.shortcuts;
     }
 
     function testSafeCanRunShortcut() public {
-        bytes32[] memory commands = new bytes32[](2);
-        bytes[] memory state = new bytes[](2);
-
+        bytes32[] memory commands = new bytes32[](1);
         commands[0] = WeirollPlanner.buildCommand(
-            token0.approve.selector,
+            SafeEnsoShortcutsTest(address(this)).testSafeCanRunShortcut.selector,
             0x01, // call
             0x0001ffffffff, // 2 inputs
             0xff, // no output
-            address(token0)
+            address(this)
         );
 
-        commands[1] = WeirollPlanner.buildCommand(
-            vault.deposit.selector,
-            0x01, // call
-            0x01ffffffffff, // 1 input
-            0xff, // no output
-            address(vault)
-        );
+        bytes[] memory state = new bytes[](1);
+        state[0] = abi.encode(0x1);
 
-        state[0] = abi.encode(address(vault));
-        state[1] = abi.encode(defaultAmount);
-
-        uint256 deadline = block.timestamp + 100;
-        IAllowanceTransfer.PermitDetails memory details = IAllowanceTransfer.PermitDetails({
-            token: address(token0),
-            amount: defaultAmount,
-            expiration: defaultExpiration,
-            nonce: defaultNonce
-        });
-        IAllowanceTransfer.PermitSingle memory permit =
-            IAllowanceTransfer.PermitSingle({details: details, spender: address(shortcuts), sigDeadline: deadline});
-        bytes memory sig = getPermitSignature(permit, fromPrivateKey, DOMAIN_SEPARATOR);
-
-        vm.startPrank(from);
-        shortcuts.permit2AndExecuteShortcut(bytes32(0), commands, state, details, deadline, sig);
-        vm.stopPrank();
-
-        uint256 depositedAmount = vault.deposits(address(shortcuts));
-        assertEq(uint256(defaultAmount), depositedAmount);
+        shortcuts.executeShortcut(bytes32(0), commands, state);
     }
 }
