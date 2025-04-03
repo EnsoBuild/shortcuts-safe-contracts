d9c1dc5b8ffd99f2e4793f73c2936d79409b1dc7
PeterMPhillips <github@pmphillips.ca>
2025-04-02 17:53:59 -0700

    update deployers for new router

--- script/ClientDeployer.s.sol
+++ script/ClientDeployer.s.sol
@@ -2,11 +2,12 @@
 pragma solidity ^0.8.28;
 
 import "forge-std/Script.sol";
-import "../src/router/EnsoShortcutRouter.sol";
+import "../src/EnsoShortcuts.sol";
+import "../src/router/EnsoRouter.sol";
 import "../src/delegate/DelegateEnsoShortcuts.sol";
 
 struct DeployerResult {
-    EnsoShortcutRouter router;
+    EnsoRouter router;
     EnsoShortcuts shortcuts;
     DelegateEnsoShortcuts delegate;
 }
@@ -17,8 +18,8 @@ contract Deployer is Script {
 
         vm.startBroadcast(deployerPrivateKey);
 
-        result.router = new EnsoShortcutRouter{salt: "EnsoShortcutRouter"}();
-        result.shortcuts = result.router.enso();
+        result.router = new EnsoRouter{salt: "EnsoRouter"}();
+        result.shortcuts = new EnsoShortcuts{salt: "EnsoShortcuts"}(address(result.router));
         result.delegate = new DelegateEnsoShortcuts{salt: "DelegateEnsoShortcuts"}();
 
         vm.stopBroadcast();
--- script/FullDeployer.s.sol
+++ script/FullDeployer.s.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.28;
 
 import "forge-std/Script.sol";
-import "../src/router/EnsoShortcutRouter.sol";
+import "../src/EnsoShortcuts.sol";
+import "../src/router/EnsoRouter.sol";
 import "../src/delegate/DelegateEnsoShortcuts.sol";
 import "../src/helpers/DecimalHelpers.sol";
 import "../src/helpers/EnsoShortcutsHelpers.sol";
@@ -14,7 +15,7 @@ import {SwapHelpers} from "../src/helpers/SwapHelpers.sol";
 import "../src/helpers/TupleHelpers.sol";
 
 struct DeployerResult {
-    EnsoShortcutRouter router;
+    EnsoRouter router;
     EnsoShortcuts shortcuts;
     DelegateEnsoShortcuts delegate;
     DecimalHelpers decimalHelpers;
@@ -27,14 +28,14 @@ struct DeployerResult {
     TupleHelpers tupleHelpers;
 }
 
-contract FullDeploy is Script {
+contract FullDeployer is Script {
     function run() public returns (DeployerResult memory result) {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
         vm.startBroadcast(deployerPrivateKey);
 
-        result.router = new EnsoShortcutRouter{salt: "EnsoShortcutRouter"}();
-        result.shortcuts = result.router.enso();
+        result.router = new EnsoRouter{salt: "EnsoRouter"}();
+        result.shortcuts = new EnsoShortcuts{salt: "EnsoShortcuts"}(address(result.router));
         result.delegate = new DelegateEnsoShortcuts{salt: "DelegateEnsoShortcuts"}();
 
         result.decimalHelpers = new DecimalHelpers{salt: "DecimalHelpers"}();
